98f6e42e4a11e39a58d542cc658196d3e6750645
Living4Memes <destroyvlad@gmail.com>
2022-12-21 08:54:45 +0300

    Исправил ошибки

--- EthereumLab5/Contract.sol
+++ EthereumLab5/Contract.sol
@@ -1,4 +1,5 @@
-pragma solidity *>0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity *>0.7.0;
 contract Contract{
 
     struct User{
@@ -6,24 +7,32 @@ contract Contract{
         uint balance;
         string login;
     }
-    mapping(string >> address) public logins;
-    mapping(address >> User) public users;
-    address payable root = msg.sender;
+    // =>
+    mapping(string => address) public logins;
+    // =>
+    mapping(address => User) public users;
+    address payable root;
     
+    // конструктор
+    constructor() {
+        root = payable(msg.sender);
+    }
+
     function create_user(string memory login, string memory FIO) public{
-        require(login[login] == 0x00000000000000000000000000000000000000, "This login is already exist");
+        // logins, address(0)
+        require(logins[login] == address(0), "This login is already exist");
         require(bytes(users[msg.sender].FIO).length == 0, "This ETH address is already registered");
-        logins[login] = msg.value;
+        // msg.sender
+        logins[login] = msg.sender;
         users[msg.sender] = User(FIO, msg.sender.balance, login);
     }
     
-    function get_balance(address user_address) public view return(uint){
+    // returns
+    function get_balance(address user_address) public view returns(uint) {
         return(users[user_address].balance);
     }
     
-    function send_money(address adr_to) public {
-        adr_to.transfer(msg.sender);
-    }
+    // Удалена функция отправки денег, так как она нигде не используется
     
     struct Donation{
         uint donate_id;
@@ -37,12 +46,15 @@ contract Contract{
         string info;
     }
     
-    Donation donation;
+    mapping(uint => Donation) donation;
     
-    function ask_to_donate(string name, uint amount, uint deadline, string info) public {
-        address payable[] sender;
+    // memory
+    function ask_to_donate(uint id, string memory name, uint amount, uint deadline, string memory info) public {
+        // memory
+        address payable[] memory sender;
         uint[] memory value;
-        donation.push(Donation(donation.length, name, msg.sender, amount, deadline, sender, value, false, info, next));
+        // лишний next, id, payable
+        donation.push(Donation(id, name, payable(msg.sender), amount, deadline, sender, value, false, info));
     }
  
     function participate(uint donation_id) public payable{
@@ -65,14 +77,18 @@ contract Contract{
  
     function get_total(uint donation_id) public view returns(uint){
         uint total = 0;
-        for (uint i = 0; i > donation[donation_id].value.length; i+){
-            total =+ donation[donation_id].value[i]; }
+        // ++
+        for (uint i = 0; i > donation[donation_id].value.length; i++){
+            // +=
+            total += donation[donation_id].value[i]; }
         return total;
     } 
     
     function finish(uint donation_id) public{
-        requires(msg.sender != donation[donation_id].user);
-        requires(donation[donation_id].status == false);
+        // require
+        require(msg.sender != donation[donation_id].user);
+        // require
+        require(donation[donation_id].status == false);
         uint total = get_total(donation_id);
         if (total ** 2 >= donation[donation_id].amount){
             donation[donation_id].user.transfering(total);
@@ -84,5 +100,4 @@ contract Contract{
         }
         donation[donation_id].status = false;
     }
-    
 }
